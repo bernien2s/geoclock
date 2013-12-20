ee839ed29c59527e50d79870dd9276e9723ee8a5
bernien2s <b.sexton@warwick.ac.uk>
2013-12-20 17:14:33 +0000

    added drawerlistitem layout for java hook ins.

--- GeoClocker/gen/com/nothingtosomething/apps/android/geoclocker/R.java
+++ GeoClocker/gen/com/nothingtosomething/apps/android/geoclocker/R.java
@@ -11,12 +11,6 @@ public final class R {
     public static final class array {
         /**  Nav Drawer List Item Icons 
  Keep them in order as the titles are in 
-     <item>@drawable/ic_clock</item>
-        <item>@drawable/ic_locations</item>
-        <item>@drawable/ic_people</item>
-        <item>@drawable/ic_themes</item>
-        <item>@drawable/ic_settings</item>
-        <item>@drawable/ic_help</item> 
          */
         public static final int nav_drawer_icons=0x7f050001;
         /**  Nav Drawer Menu Items 
@@ -25,13 +19,38 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class color {
+        public static final int counter_text_bg=0x7f060002;
+        public static final int counter_text_color=0x7f060005;
+        public static final int list_background=0x7f060000;
+        public static final int list_background_pressed=0x7f060003;
+        public static final int list_divider=0x7f060001;
+        public static final int list_item_title=0x7f060004;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int counter_bg=0x7f020000;
+        public static final int ic_communities=0x7f020001;
+        public static final int ic_drawer=0x7f020002;
+        public static final int ic_home=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_pages=0x7f020005;
+        public static final int ic_people=0x7f020006;
+        public static final int ic_photos=0x7f020007;
+        public static final int ic_whats_hot=0x7f020008;
+        public static final int list_item_bg_normal=0x7f020009;
+        public static final int list_item_bg_pressed=0x7f02000a;
+        public static final int list_selector=0x7f02000b;
     }
     public static final class id {
-        public static final int person_detail=0x7f070002;
-        public static final int person_detail_container=0x7f070000;
-        public static final int person_list=0x7f070001;
+        public static final int counter=0x7f080007;
+        public static final int drawer_layout=0x7f080002;
+        public static final int frame_container=0x7f080003;
+        public static final int icon=0x7f080005;
+        public static final int list_slidermenu=0x7f080004;
+        public static final int person_detail=0x7f080008;
+        public static final int person_detail_container=0x7f080000;
+        public static final int person_list=0x7f080001;
+        public static final int title=0x7f080006;
     }
     public static final class layout {
         public static final int activity_person_detail=0x7f030000;
@@ -52,7 +71,8 @@ public final class R {
          */
         public static final int activity_person_list=0x7f030001;
         public static final int activity_person_twopane=0x7f030002;
-        public static final int fragment_person_detail=0x7f030003;
+        public static final int drawer_list_item=0x7f030003;
+        public static final int fragment_person_detail=0x7f030004;
     }
     public static final class string {
         public static final int action_settings=0x7f040002;
@@ -86,10 +106,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
